55e7ff0ff318f58a0b3d5ffe53cdc5e76760a246
tinapetkovsek <tina1.petkovsek@gmail.com>
2024-04-17 18:00:46 +0200

    dodala  drzave

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import { useEffect, useState } from "react";
 import Random from "./Random";
 import { Input } from "./components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function App() {
   const [fact, setFact] = useState({});
   const [number, setNumber] = useState(0);
+  const [countries, setCountries] = useState([
+    "Slovenia",
+    "France",
+    "Germany",
+    "Italy",
+    "Spain",
+  ]);
+  const [colors, setColors] = useState([
+    "green",
+    "red",
+    "blue",
+    "pink",
+    "orange",
+  ]);
 
   async function getRandomFact() {
     const response = await fetch("http://numbersapi.com/random?json");
@@ -18,8 +39,17 @@ export default function App() {
     setFact(data);
   }
 
+  async function getCountries() {
+    const response = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flag,borders,region",
+    );
+    const data = await response.json();
+    setCountries(data);
+  }
+
   useEffect(() => {
     getRandomFact();
+    getCountries();
   }, []);
 
   useEffect(() => {
@@ -28,6 +58,33 @@ export default function App() {
 
   return (
     <div className="container">
+      <h1 className="p-6 text-xl">My Numbers and Countries</h1>
+
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a region" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="Europe">Europe</SelectItem>
+          <SelectItem value="Asia">Asia</SelectItem>
+          <SelectItem value="Africa">Africa</SelectItem>
+          <SelectItem value="North America">North America</SelectItem>
+          <SelectItem value="South America">South America</SelectItem>
+          <SelectItem value="Oceania">Oceania</SelectItem>
+        </SelectContent>
+      </Select>
+
+      {countries
+        //.filter((country) => country.length <= 5) //izpisemo le drzave, ki imajo 5 ali manj crk
+        .filter((country) => country.region == "Europe")
+        .map((country) => (
+          <p>{country.name.common}</p>
+        ))}
+
+      {colors.map((color) => (
+        <p>{color}</p>
+      ))}
+
       <Input
         placeholder="Vnesi stevilo, ki te zanima..."
         type="number"
